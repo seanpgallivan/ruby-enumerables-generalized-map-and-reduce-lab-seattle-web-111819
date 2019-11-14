2f9cd3d6346e4e868eb048bdff50a4463df460aa
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 21:23:52 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new << yield(array[i])
+    i += 1
+  end
+  new
+end
+
+def reduce(array, start=0)
+  tot = start
+  i = 0
+  while i < array.length do
+    if array[i]
+      tot = yield(array[i], tot)
+    else
+      tot = yield(tot, array[i])
+    end
+    i += 1
+  end
+  tot
+end
