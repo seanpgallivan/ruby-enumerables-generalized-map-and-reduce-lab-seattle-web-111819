7f196c7a71afbc40c3cbfcfd3b6b4c6152a8deee
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 20:39:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,14 +8,14 @@ def map(array)
   new
 end
 
-def reduce(source_array, start=0)
-  puts "array: " + source_array.to_s
+def reduce(array, start=0)
+  puts "array: " + array.to_s
   tot = start
   i = 0
-  while i < source_array.length do
+  while i < array.length do
     puts i.to_s + " tot: " + tot.to_s
-    puts i.to_s + " arr: " + source_array[i].to_s
-    tot = yield(tot, source_array[i])
+    puts i.to_s + " arr: " + array[i].to_s
+    tot = yield(tot, array[i])
     puts tot
     i += 1
   end
