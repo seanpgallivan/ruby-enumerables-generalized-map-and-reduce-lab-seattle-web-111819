f50f94f972ac836fc95e1f7fc73fe32a0c140dc7
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 21:23:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,19 +9,14 @@ def map(array)
 end
 
 def reduce(array, start=0)
-  puts "array: " + array.to_s
   tot = start
   i = 0
   while i < array.length do
-    puts i.to_s + " tot: " + tot.to_s
-    puts i.to_s + " arr: " + array[i].to_s
-    puts i.to_s + " boo: " + (tot || array[i]).to_s
     if array[i]
       tot = yield(array[i], tot)
     else
       tot = yield(tot, array[i])
     end
-    puts i.to_s + " new: " + tot.to_s
     i += 1
   end
   tot
